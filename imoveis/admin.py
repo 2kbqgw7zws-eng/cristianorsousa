f655776c2eb2a65e47c3d25d726586f3fb672149
Cristiano Sousa <cristianosousa@MacBook-Pro-de-Cristiano.local>
2025-12-28 14:49:09 -0300

    Adicionando modelo de despesas

--- imoveis/admin.py
+++ imoveis/admin.py
@@ -1,31 +1,17 @@
 from django.contrib import admin
-from django.shortcuts import redirect # Importante para o redirecionamento
-from .models import Imovel, Locacao, RelatorioGeral
+from .models import Imovel, Locacao, Despesa # <--- Adicionei Despesa aqui
 
-# Configuração do Imóvel
+@admin.register(Imovel)
 class ImovelAdmin(admin.ModelAdmin):
-    list_display = ('nome', 'valor_diaria', 'status')
-    list_editable = ('status',)
+    list_display = ('nome',)
 
-# Configuração da Locação
+@admin.register(Locacao)
 class LocacaoAdmin(admin.ModelAdmin):
-    list_display = ('imovel', 'cliente', 'cpf', 'telefone', 'data_entrada', 'data_saida', 'valor_cobrado_diaria')
+    list_display = ('imovel', 'data_entrada', 'data_saida', 'valor_cobrado_diaria')
     list_filter = ('imovel', 'data_entrada')
-    search_fields = ('cliente', 'cpf', 'telefone')
 
-# Configuração do Botão de Relatório
-class RelatorioGeralAdmin(admin.ModelAdmin):
-    def changelist_view(self, request, extra_context=None):
-        # Quando clicar no botão, redireciona para a URL do relatório
-        return redirect('/relatorio/')
-
-    def has_add_permission(self, request):
-        return False # Remove botão "Add"
-    
-    def has_change_permission(self, request, obj=None):
-        return False # Remove botão "Change"
-
-# Registrando tudo
-admin.site.register(Imovel, ImovelAdmin)
-admin.site.register(Locacao, LocacaoAdmin)
-admin.site.register(RelatorioGeral, RelatorioGeralAdmin)
+@admin.register(Despesa) # <--- Novo registro
+class DespesaAdmin(admin.ModelAdmin):
+    list_display = ('imovel', 'categoria', 'data_pagamento', 'valor')
+    list_filter = ('imovel', 'categoria', 'data_pagamento')
+    search_fields = ('descricao',)
