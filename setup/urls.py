b670b746a4563b39ad179d60563a7aaad8ccd38b
Cristiano Sousa <cristianosousa@MacBook-Pro-de-Cristiano.local>
2025-12-28 12:41:38 -0300

    Redirecionando home para admin

--- setup/urls.py
+++ setup/urls.py
@@ -1,24 +1,11 @@
-"""
-URL configuration for setup project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path
-from imoveis.views import relatorio_financeiro # <--- Importe a função
+from django.views.generic import RedirectView # <--- Importação nova
+from imoveis.views import relatorio_geral
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('relatorio/', relatorio_financeiro), # <--- Crie o link
+    path('relatorio/', relatorio_geral, name='relatorio_geral'),
+    # Essa linha abaixo redireciona a home vazia para o /admin automaticamente
+    path('', RedirectView.as_view(url='/admin/')), 
 ]
