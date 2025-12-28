ec89bf70fd36127650c7fa4ba21c8480a786f4a1
Cristiano Sousa <cristianosousa@MacBook-Pro-de-Cristiano.local>
2025-12-28 15:39:30 -0300

    Ajuste do redirecionamento do admin para o relatório

--- imoveis/admin.py
+++ imoveis/admin.py
@@ -1,21 +1,10 @@
-from django.contrib import admin
-from .models import Imovel, Locacao, Despesa, RelatorioGeral # Certifique-se de importar todos
+from django.shortcuts import redirect # Adicione esta importação no topo do arquivo
 
-@admin.register(Imovel)
-class ImovelAdmin(admin.ModelAdmin):
-    list_display = ('nome', 'valor_diaria', 'status')
-
-@admin.register(Locacao)
-class LocacaoAdmin(admin.ModelAdmin):
-    list_display = ('imovel', 'cliente', 'data_entrada', 'data_saida')
-
-@admin.register(Despesa)
-class DespesaAdmin(admin.ModelAdmin):
-    list_display = ('imovel', 'categoria', 'data_pagamento', 'valor')
-    list_filter = ('imovel', 'categoria')
-
-# ESTE BLOCO ABAIXO É O QUE CRIA O BOTÃO DO RELATÓRIO
 @admin.register(RelatorioGeral)
 class RelatorioGeralAdmin(admin.ModelAdmin):
+    def changelist_view(self, request, extra_context=None):
+        # Redireciona o usuário para a URL que definimos no urls.py
+        return redirect('/relatorio/') 
+
     def has_add_permission(self, request): return False
     def has_delete_permission(self, request, obj=None): return False
