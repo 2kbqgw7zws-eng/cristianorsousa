08fbc3872b7bd1ec6966755b4d378cc78df84dc9
Cristiano Sousa <cristianosousa@MacBook-Pro-de-Cristiano.local>
2025-12-28 15:43:29 -0300

    Fix admin import error

--- imoveis/admin.py
+++ imoveis/admin.py
@@ -1,10 +1,23 @@
-from django.shortcuts import redirect # Adicione esta importação no topo do arquivo
+from django.contrib import admin # <--- ESSA LINHA É O QUE ESTÁ FALTANDO
+from django.shortcuts import redirect
+from .models import Imovel, Locacao, Despesa, RelatorioGeral
+
+@admin.register(Imovel)
+class ImovelAdmin(admin.ModelAdmin):
+    list_display = ('nome',)
+
+@admin.register(Locacao)
+class LocacaoAdmin(admin.ModelAdmin):
+    list_display = ('imovel', 'cliente', 'data_entrada', 'data_saida')
+
+@admin.register(Despesa)
+class DespesaAdmin(admin.ModelAdmin):
+    list_display = ('imovel', 'categoria', 'data_pagamento', 'valor')
 
 @admin.register(RelatorioGeral)
 class RelatorioGeralAdmin(admin.ModelAdmin):
     def changelist_view(self, request, extra_context=None):
-        # Redireciona o usuário para a URL que definimos no urls.py
-        return redirect('/relatorio/') 
+        return redirect('/relatorio/')
 
     def has_add_permission(self, request): return False
     def has_delete_permission(self, request, obj=None): return False
