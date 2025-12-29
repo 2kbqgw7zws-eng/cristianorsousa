3ddd72ca803a9bb1d890b4080c9d4f2b24d5c4c7
Cristiano Sousa <cristianosousa@MacBook-Pro-de-Cristiano.local>
2025-12-29 11:19:45 -0300

    Correção de layout e Excel com duas abas

--- advocacia/views.py
+++ advocacia/views.py
@@ -8,15 +8,13 @@ def download_advocacia_excel(request):
     ws1 = wb.active
     ws1.title = "Faturamento"
     ws1.append(['DATA', 'CLIENTE', 'VALOR'])
-    faturamentos = FaturamentoAdvocacia.objects.filter(data__year=ano)
-    for f in faturamentos:
+    for f in FaturamentoAdvocacia.objects.filter(data__year=ano):
         ws1.append([f.data.strftime('%d/%m/%Y'), f.cliente, float(f.valor)])
     
-    # Aba de Despesas (Cria uma nova planilha no mesmo arquivo)
+    # Aba de Despesas
     ws2 = wb.create_sheet(title="Despesas")
     ws2.append(['DATA', 'DESCRIÇÃO', 'LOCAL', 'VALOR'])
-    despesas = DespesaAdvocacia.objects.filter(data__year=ano)
-    for d in despesas:
+    for d in DespesaAdvocacia.objects.filter(data__year=ano):
         ws2.append([d.data.strftime('%d/%m/%Y'), d.descricao, d.local, float(d.valor)])
         
     response = HttpResponse(content_type='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
